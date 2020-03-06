97d232e420b55128a4785bbe2beb099997881f89
YourName <you@example.com>
2020-03-06 20:08:48 +0900

    add-routing

---
+++ app/controllers/itemimages_controller.rb
@@ -0,0 +1,2 @@
+class ItemimagesController < ApplicationController
+end
---
+++ test/controllers/solditems_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SolditemsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
