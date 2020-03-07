0a439aa35dc66afa7502e35f6edc14659a72f032
yohei-koba-tokyo <60637308+yohei-koba-tokyo@users.noreply.github.com>
2020-03-07 11:48:44 +0900

    rails_s時のエラーは解決。デプロイはこれから。一度コミットします。

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,15 +3,19 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'ap-northeast-1'
-  }
+  if Rails.env.development? || Rails.env.test? #開発とテストは今まで通りに
+    config.storage = :file
+  elsif Rails.env.production? #本番はS3に保存する 
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',  #AWSのアクセスキーとシークレットキーを環境変数で定義する
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],  #credentails.ymlに鍵の本体がある？
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],   #credentails.ymlに鍵の本体がある？
+      region: 'ap-northeast-1'   #'AWSで設定した地域（おそらく'ap-northeast-1') '
+    }
 
-  config.fog_directory  = 'freemarket2020b'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket2020b'
+    config.fog_directory  = 'freemarket2020b'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket2020b'
+  end
 end
