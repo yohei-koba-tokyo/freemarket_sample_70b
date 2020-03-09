4a047955f84f5f9ab9eb9ee0323a9db10cb2588b
yohei-koba-tokyo <60637308+yohei-koba-tokyo@users.noreply.github.com>
2020-03-09 19:58:55 +0900

    routing、変更（DB変更に応じての変更）itemimages等ネストしてます

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
-  resources :addresses
-  resources :items
-  resources :itemimages
+  resources :items do
+    resources :itemimages
+    resources :categories
+  end
   resources :solditems
-  resources :categories
-  resources :item_categories
   resources :comments
   resources :likes
   resources :users
   resources :users do
-    resources :creditcards
+    resources :credit
+    resources :addresses
   end
 end
 
