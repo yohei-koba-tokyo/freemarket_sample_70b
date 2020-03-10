810dfe7f75a109b2863d9e12562e6d4615c300ea
accttakaya <takaya.satou.0206@gmail.com>
2020-03-10 13:32:18 +0900

    itemsテーブルのcreate機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,9 +3,19 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @items = Item.new
   end
 
   def create
+    binding.pry
+    Item.create(item_params)
   end
-  
+
+
+  private
+  def item_params
+    params.require(:item).permit(:name,:explanation,:category,:brand,:condition,:postage,:area,:day,:price).merge(user_id:1)
+  end
+
 end
+
