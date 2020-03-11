1f20e8c16c7b19e898982a63de378d28e99fecdc
Github-Tsubasa <59303334+Github-Tsubasa@users.noreply.github.com>
2020-03-11 20:33:21 +0900

    put in login code 20:33 march 11

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth, if: :production?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:last_name,:first_name,:last_name_kana,:first_name_kana,:brithday,:profile])
+  end
   private
 
   def production?
