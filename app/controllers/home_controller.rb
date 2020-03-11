da8e8327cd6dd289f20fba35a25dbfec2bc1e861
kazuho0531 <kazuho.053187@gmail.com>
2020-03-11 20:02:05 +0900

    リンクの実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,33 @@
 class HomeController < ApplicationController
   def index
+    @items = []
+    vvv = []
+    @bland_items = []
+    zzz = []
+
+    Item.all.each do |i|
+      if i.status == 1
+        vvv << i
+      end
+    end
+
+    Item.all.each do |i|
+      if i.status == 1 && i.brand == "ユニクロ"
+        zzz << i
+      end
+    end
+
+    i = 0
+    vvv.reverse_each do |value|
+      i += 1
+      @items << value
+      break if i == 3
+    end
+
+    zzz.reverse_each do |value|
+      i += 1
+      @bland_items << value
+      break if i == 3
+    end 
   end
 end
