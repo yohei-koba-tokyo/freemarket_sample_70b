6ae1fd8c9647649d0f0dee583c8c53d8501a0871
kazuho0531 <kazuho.053187@gmail.com>
2020-03-12 14:28:18 +0900

    不要な配列定義の削除

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @items = []
-    @brand_items = []
-
     @items = Item.select { |item| item.status == 1 }.last(3)
     @brand_items = Item.select { |item| item.status == 1 && item.brand == "ユニクロ" }.last(3)
   end
