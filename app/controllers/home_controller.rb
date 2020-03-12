668999f25f89f51eb9db907a02bcd64ffeb9027a
kazuho0531 <kazuho.053187@gmail.com>
2020-03-12 10:50:44 +0900

    一旦コミット

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,33 +1,9 @@
 class HomeController < ApplicationController
   def index
     @items = []
-    vvv = []
-    @bland_items = []
-    zzz = []
+    @brand_items = []
 
-    Item.all.each do |i|
-      if i.status == 1
-        vvv << i
-      end
-    end
-
-    Item.all.each do |i|
-      if i.status == 1 && i.brand == "ユニクロ"
-        zzz << i
-      end
-    end
-
-    i = 0
-    vvv.reverse_each do |value|
-      i += 1
-      @items << value
-      break if i == 3
-    end
-
-    zzz.reverse_each do |value|
-      i += 1
-      @bland_items << value
-      break if i == 3
-    end 
+    @items = Item.select { |item| item.status == 1 }.last(3)
+    @brand_items = Item.select { |item| item.status == 1 && item.brand == "ユニクロ" }.last(3)
   end
 end
