52a8f3271572d75be7cac43a15d8fc1da4a64c22
yohei-koba-tokyo <60637308+yohei-koba-tokyo@users.noreply.github.com>
2020-03-12 20:37:43 +0900

    カード登録に必要な記述の終了後,エラー解決を図ります()
    Payjp::InvalidRequestError in CreditsController#create
    
    2
    23
    24
    25
    26
    27
    
        else
          # トークンが正常に発行されていたら、顧客情報をPAY.JPに登録します。
          customer = Payjp::Customer.create(
            description: 'test', # 無くてもOK。PAY.JPの顧客情報に表示する概要です。
            email: current_user.email,
            credit: params['payjp-token'], # 直前のnewアクションで発行され、送られてくるトークンをここで顧客に紐付けて永久保存します。

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -9,10 +9,13 @@ class CreditsController < ApplicationController
     redirect_to action: "index" if credit.present?
   end
 
+  def index 
+  end
  # indexアクションはここでは省略
 
   def create #Payjpとcreditのデータベースを作成
     Payjp.api_key = 'sk_test_6fdc4d02560876ba18e46565'
+    # テスト用の秘密鍵を入れてあります
 
     if params['payjp-token'].blank?
       redirect_to action: "new"
@@ -22,7 +25,7 @@ class CreditsController < ApplicationController
         description: 'test', # 無くてもOK。PAY.JPの顧客情報に表示する概要です。
         email: current_user.email,
         credit: params['payjp-token'], # 直前のnewアクションで発行され、送られてくるトークンをここで顧客に紐付けて永久保存します。
-        # metadata: {user_id: current_user.id} # 無くてもOK。
+        metadata: {user_id: current_user.id} # 無くてもOK。
       )
       @credit = Credit.new(user_id: current_user.id, customer_id: customer.id, credit_id: customer.default_credit)
       if @credit.save
