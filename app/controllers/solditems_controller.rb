256c9403081a5e7f3d0c574101733ee115bc310b
kazuho0531 <kazuho.053187@gmail.com>
2020-03-17 15:03:28 +0900

    「現在のユーザーの出品 且つ 出品中商品」の表示実装

--- app/controllers/solditems_controller.rb
+++ app/controllers/solditems_controller.rb
@@ -1,6 +1,6 @@
 class SolditemsController < ApplicationController
   def index
     # @items = Item.select { |item| item.status == 1 }
-    @items = Item.select { |item| item.user_id == current_user.id } and Item.select { |item| item.status == 1 }
+    @items = Item.select { |item| item.user_id == current_user.id && item.status == 1 }
   end
 end
