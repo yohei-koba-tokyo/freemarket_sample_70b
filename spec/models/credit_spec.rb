e5372c893b19247d0171b0b7a0765e7315c40de5
yohei-koba-tokyo <60637308+yohei-koba-tokyo@users.noreply.github.com>
2020-03-18 21:26:00 +0900

    中間コミット

--- spec/models/credit_spec.rb
+++ spec/models/credit_spec.rb
@@ -1,15 +1,9 @@
 require 'rails_helper'
-# RSpec.describe Credit, do
-
 describe Credit do
   describe '#create'do
-
     it "can save" do
       credit = build(:credit)
       expect(credit).to be_valid
     end
-
-
-
   end
 end
