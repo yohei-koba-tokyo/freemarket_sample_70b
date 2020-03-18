0d860e806a56385434a8d299682620bd5309224f
Github-Tsubasa <59303334+Github-Tsubasa@users.noreply.github.com>
2020-03-18 15:34:22 +0900

    一時コミット

--- app/models/address.rb
+++ app/models/address.rb
@@ -9,8 +9,9 @@ class Address < ApplicationRecord
   validates :prefecture, presence: true
   validates :municipality, presence: true
   validates :address, presence: true
+  validates :birthday, presence: true
 
   validates :postcode, format: { with: /\A\d{7}\z/ }
   VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
-  validates :phone, presence: true, format: { with: VALID_PHONE_REGEX }
+  validates :phone, format: { with: VALID_PHONE_REGEX }
 end
