e29cd59c6d3238eb72c062dc36094d824c415f96
Github-Tsubasa <59303334+Github-Tsubasa@users.noreply.github.com>
2020-03-20 17:50:13 +0900

    一時コミット　textarea実装ほか

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+  def create
+    comment = Comment.create(comment_params)
+    redirect_to "/tweets/#{comment.tweet.id}"  
+  end
+  
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
+  end
+  
+  def show
+    @comment = Comment.new
+    @comments = @tweet.comments.includes(:user)
+  end
+
 end
