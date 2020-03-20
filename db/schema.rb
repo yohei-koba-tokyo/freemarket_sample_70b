8de8f287cf16ba906a9acfb21cbcf18045bbf94b
yohei-koba-tokyo <60637308+yohei-koba-tokyo@users.noreply.github.com>
2020-03-20 14:53:30 +0900

    config修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_13_102755) do
+ActiveRecord::Schema.define(version: 2020_03_18_050332) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postcode", null: false
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2020_03_13_102755) do
     t.string "municipality", null: false
     t.string "address", null: false
     t.string "room_number"
-    t.integer "phone", null: false
+    t.string "phone", null: false
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
