5354cc675089ea93cefeae436f22cc0a54cafa18
yohei-koba-tokyo <60637308+yohei-koba-tokyo@users.noreply.github.com>
2020-03-20 14:41:28 +0900

    credits controller test

--- spec/controllers/credits_controller_spec.rb
+++ spec/controllers/credits_controller_spec.rb
@@ -2,24 +2,19 @@ require 'rails_helper'
 describe CreditsController do
   let(:user) { create(:user) }
   
-  
   describe 'GET #new' do
-
     context 'login時' do
       before do
         login_user user
       end
-
       it "newアクション" do
-        # allow(Payjp::Customer).to receive(:create).and_return(PayjpMock.prepare_customer_information)
+        allow(Payjp::Customer).to receive(:create).and_return(PayjpMock.prepare_customer_information)
         user.credit = create(:credit, user_id: user.id)
         get :new
         expect(response).to redirect_to(credits_path)
-        # expect(response).to render_template :index
       end
     end
   end
-
 
   describe 'GET #index' do
     context 'login時' do
@@ -28,12 +23,10 @@ describe CreditsController do
       end
       it "indexアクション" do
         credit = create(:credit, user_id: user.id)
-        # allow(Payjp::Customer).to receive(:create).and_return(PayjpMock.prepare_customer_information)
+        allow(Payjp::Customer).to receive(:create).and_return(PayjpMock.prepare_customer_information)
         get :index, params: {id: credit}
-        # expect(assigns(:credit)).to match(credit)
         expect(assigns(:credit)).to eq credit
       end
-
     end
   end
 
@@ -43,19 +36,10 @@ describe CreditsController do
         login_user user
       end
       it "destroyアクション" do
-      
         credit = create(:credit, user_id: user.id)
         delete :destroy, params: {id: credit}
-        # expect(assigns(:credit)).to eq credit
-      
       end
-
-
     end
   end
-
-
-
-
 
 end
