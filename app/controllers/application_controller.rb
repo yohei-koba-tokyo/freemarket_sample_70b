32fa6e7d6c3680536ba5ffb79a2177829f781ead
yohei-koba-tokyo <60637308+yohei-koba-tokyo@users.noreply.github.com>
2020-03-25 19:14:11 +0900

    basic認証の不具合修正１

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,7 +35,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == Rails.application.credentials[:BASIC_AUTH_USER] && password == Rails.application.credentials[:BASIC_AUTH_PASSWORD]
     end
   end
 end
